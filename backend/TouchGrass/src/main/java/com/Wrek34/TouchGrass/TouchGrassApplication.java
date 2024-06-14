964ae66ef2efb846f332a2527770981c3ca1a7b4
Ryan Wieczorek (Wrek) <raw2190@columbia.edu>
2024-06-13 21:59:11 -0400

    Backend for TouchGrass initial -Wrek

---
+++ backend/TouchGrass/src/main/java/com/Wrek34/TouchGrass/TouchGrassApplication.java
@@ -0,0 +1,13 @@
+package com.Wrek34.TouchGrass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouchGrassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouchGrassApplication.class, args);
+	}
+
+}
---
+++ backend/TouchGrass/src/test/java/com/Wrek34/TouchGrass/TouchGrassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Wrek34.TouchGrass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouchGrassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
